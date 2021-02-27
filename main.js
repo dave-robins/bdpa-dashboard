06f131fc5bba0b8c11d0d04884f354d78472ae9c
dave-robins <dave.a.robins@gmail.com>
2021-02-27 17:34:33 -0600

    navbar

--- main.js
+++ main.js
@@ -15,32 +15,37 @@ function setGreeting() {
 }
 
 function currentTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
 
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
 
-    document.getElementById("clock").innerHTML = 
-      hour + " : " + min + " : " + sec
+  document.getElementById("clock").innerHTML = 
+    hour + " : " + min + " : " + sec
 
-    setTimeout(() => { currentTime() }, 1000)
-  }
+  setTimeout(() => { currentTime() }, 1000)
+}
 
-  function updateTime(time) {
-    return time < 10 ? ("0" + time) : time
-  }
+function updateTime(time) {
+  return time < 10 ? ("0" + time) : time
+}
   
-  async function getRandomQuote() {
-      var result = await fetch("https://api.quotable.io/random")
-      .then(res => res.json())
+async function getRandomQuote() {
+  var result = await fetch("https://api.quotable.io/random")
+  .then(res => res.json())
 
-      document.getElementById("quote-body").innerHTML = result.content
-      document.getElementById("quote-footer").innerHTML = result.author
-    }
+  document.getElementById("quote-body").innerHTML = result.content
+  document.getElementById("quote-footer").innerHTML = result.author
+}
+
+function toggle() {
+  document.getElementById()
+  console.log("toggled")
+}
 
 
 setGreeting()
